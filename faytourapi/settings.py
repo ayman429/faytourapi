1e279b70f0c1c9687b6893cbc8a780b4d0e18792
ayman mansour <am5432@fayoum.edu.eg>
2023-06-23 22:24:31 +0200

    main

--- faytourapi/settings.py
+++ faytourapi/settings.py
@@ -67,8 +67,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     
+    
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ]
     }
 
